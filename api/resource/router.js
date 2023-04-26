d773144d86532a8a63f91940b3eca1ea556c686a
oguzhanalper06 <alperhanoguz06@gmail.com>
2023-04-26 17:54:18 +0300

    task oluşturuldu

--- api/resource/router.js
+++ api/resource/router.js
@@ -1,1 +1,23 @@
 // `/api/resources` router buraya
+const express = require("express");
+const router = express.Router();
+const Resource = require("./model");
+// const mw = require("./middleware");
+router.get("/", async (req, res, next) => {
+  try {
+    const resourse = await Resource.getAll();
+    res.json(resourse);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/", async (req, res, next) => {
+  try {
+    const newResource = await Resource.insert(req.body);
+    res.status(200).json(newResource);
+  } catch (error) {
+    next(error);
+  }
+});
+module.exports = router;
--- api/task/router.js
+++ api/task/router.js
@@ -1,1 +1,23 @@
 // `/api/tasks` router buraya
+const express = require("express");
+const Tasks = require("./model");
+const router = express.Router();
+const mw = require("./middleware");
+router.get("/", async (req, res, next) => {
+  try {
+    const task = await Tasks.getAll();
+    res.json(task);
+  } catch (error) {
+    next(error);
+  }
+});
+router.post("/", mw.checkPayloadAndProjectIdExist, async (req, res, next) => {
+  try {
+    let insertedData = await Tasks.insert(req.body);
+    res.json(insertedData);
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;
