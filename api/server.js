946fe60bd423cd4a9cfa705b671f9e4ec5d4beca
oguzhanalper06 <alperhanoguz06@gmail.com>
2023-04-26 17:34:58 +0300

    server.js oluşturuldu

--- api/server.js
+++ api/server.js
@@ -1,1 +1,21 @@
-// serverı buraya yazın ve index.js require yazın
+const express = require("express");
+const server = express();
+server.use(express.json());
+
+const projectsRouter = require("./project/router");
+server.use("/api/projects", projectsRouter);
+
+const resourceRouter = require("./resource/router");
+server.use("/api/resources", resourceRouter);
+
+const taskRouter = require("./task/router");
+server.use("/api/tasks", taskRouter);
+
+server.get("/", () => {
+  res.status(200).json({
+    statusCode: 200,
+    message: "Server is up and runing",
+  });
+});
+
+module.exports = server;
