ec5ec71b9ddbf5883f5f2f232385ebcc98f336ac
oguzhanalper06 <alperhanoguz06@gmail.com>
2023-04-26 17:26:30 +0300

    index.js dosyası oluşturuldu

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Server'ı buradan başlatın
+const server = require("./api/server");
+
+const port = process.env.PORT || 9000;
+
+server.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
